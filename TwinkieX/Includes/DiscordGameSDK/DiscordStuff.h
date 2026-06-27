4809cd00b2baca061f5e1606c8bd42359abf304a
CodyNinja1 <jailmanguy1@gmail.com>
2026-06-27 12:03:45 +0300

    Add remaining appids

--- TwinkieX/Includes/DiscordGameSDK/DiscordStuff.h
+++ TwinkieX/Includes/DiscordGameSDK/DiscordStuff.h
@@ -8,11 +8,15 @@
 #ifdef TMCN
 #define DISCORD_APP_ID 1520334169883017348ULL
 #elif defined(TM1)
+#define DISCORD_APP_ID 1520353467095449681ULL
 #elif defined(TMO)
 #define DISCORD_APP_ID 1520334569847521390ULL
 #elif defined(TMS)
+#define DISCORD_APP_ID 0ULL
 #elif defined(TMSX)
+#define DISCORD_APP_ID 1520352680260931645ULL
 #elif defined(ESWC)
+#define DISCORD_APP_ID 1520353733261656134ULL
 #else
 #error "No target game defined."
 #endif
